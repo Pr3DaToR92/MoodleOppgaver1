33df2919428f83c5231b6ac247e0df6f54c215be
Kevin Tunge <kevintunge12@hotmail.com>
2024-12-04 14:30:36 +0100

    Added "Innkapsling.cs" and updated repos.

---
+++ ConsoleApp5/Innkapsling.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodleOppgaver1
+{
+    internal class Innkapsling
+    {
+        private string _name;
+
+        public void SetName(string newName)
+        {
+            _name = newName;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void CreateName()
+        {
+            var person1 = new Innkapsling();
+            person1.SetName("Per");
+
+            Console.WriteLine("Navn: " + person1.GetName());
+        }
+    }
+    
+}
---
+++ ConsoleApp5/list.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodleOppgaver1
+{
+    internal class list
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public list(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+
+}
